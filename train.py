4e6a8911c9f79c4823d5d6845e11b1e5173c05a1
brandonlou <14984696+brandonlou@users.noreply.github.com>
2021-07-08 15:28:02 -0700

    Try SelectKBest

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import argparse
 import pickle
 from lightgbm import LGBMClassifier
 from numpy import array, mean, std
-from sklearn.feature_selection import RFE
+from sklearn.feature_selection import RFE, SelectKBest, f_classif
 from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, \
                             precision_score, recall_score, roc_auc_score
 from sklearn.model_selection import KFold
@@ -16,14 +16,14 @@ NUM_LEAVES = 2048
 MAX_DEPTH = 15
 # MIN_DATA_IN_LEAF = 50
 # FEATURE_FRACTION = 0.5
-FEATURES_TO_SELECT = 500
+FEATURES_TO_SELECT = 150
 
 
 def get_model():
     classifier = LGBMClassifier(boosting_type=BOOSTING, num_leaves=NUM_LEAVES,
                            max_depth=MAX_DEPTH, learning_rate=LEARNING_RATE,
                            objective=OBJECTIVE)
-    # rfe = RFE(classifier, n_features_to_select=FEATURES_TO_SELECT, step=1)
+    #rfe = RFE(classifier, n_features_to_select=FEATURES_TO_SELECT, step=1)
     return classifier
 
 
@@ -37,6 +37,10 @@ def main():
         data = pickle.load(feature_file)
     X_data = array(data[0])
     y_data = array(data[1])
+
+    # Feature selection using ANOVA F measure
+    fs = SelectKBest(score_func=f_classif, k=FEATURES_TO_SELECT)
+    X_data = fs.fit_transform(X_data, y_data)
 
     accuracy = list()
     precision = list()
@@ -61,7 +65,7 @@ def main():
         auc.append(roc_auc_score(y_test, y_pred))
         f1.append(f1_score(y_test, y_pred))
         tn, fp, _, _ = confusion_matrix(y_test, y_pred).ravel()
-        fp_rate.append(fp / (fp + tn) * 100)
+        fp_rate.append(fp / (fp + tn))
 
         print(f'Fold {current_fold} complete')
         current_fold += 1
@@ -70,7 +74,7 @@ def main():
     print(f'Precision: {round(mean(precision), 3):.3f}')
     print(f'Recall: {round(mean(recall), 3):.3f}')
     print(f'AUC: {round(mean(auc), 3):.3f}')
-    print(f'FP Rate: {round(mean(fp_rate), 3):.3f}%')
+    print(f'FP Rate: {round(mean(fp_rate), 3):.3f}')
     print(f'F1 Score: {round(mean(f1), 3):.3f}')
 
     # Create a single model fitted from all available data.
