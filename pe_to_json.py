3c09c53bd984c57ecb4f99452429528a4be30a49
brandonlou <14984696+brandonlou@users.noreply.github.com>
2021-07-08 15:27:05 -0700

    Add PE to JSON script

---
+++ pe_to_json.py
@@ -0,0 +1,49 @@
+import json
+import os
+import sys
+from ember import PEFeatureExtractor
+import py
+
+
+input_dir = 'samples/malign'
+output_dir = 'malign'
+
+
+def main():
+    # Capture all stderr from lief (C++ module)
+    capture = py.io.StdCaptureFD(out=False, in_=False)
+   
+    pe_to_json = dict() 
+    extractor = PEFeatureExtractor(feature_version=2, print_feature_warning=False)
+   
+    # Extract ember features 
+    for pe_file in os.listdir(input_dir):
+        pe_filename = f'{input_dir}/{pe_file}'
+        if os.path.getsize(pe_filename) > 2097152: # 2 MiB
+            continue
+        with open(pe_filename, 'rb') as in_file:
+            sys.stdout.write(f'FILE: {pe_file}\n')
+            sys.stderr.write(f'FILE: {pe_file}\n')
+            sys.stderr.flush()
+            features_dict = extractor.raw_features(in_file.read())
+            features_dict['ember_errors'] = []
+            pe_to_json[pe_file] = features_dict
+    
+    # Append lief/ember warnings to use as features
+    out, err = capture.reset()
+    filename = ''
+    for line in err.splitlines():
+        if line[0:5] == 'FILE:':
+            filename = line[6:]
+        else:
+            pe_to_json[filename]['ember_errors'].append(line)
+
+    # Save to JSON file
+    for pe_file in pe_to_json:
+        with open(f'{output_dir}/{pe_file}.json', 'wt') as out_file:
+            json.dump(pe_to_json[pe_file], out_file, indent=4)
+
+
+if __name__ == '__main__':
+    main()
+
