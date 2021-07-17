888b3a71ab63131cc7ea7ce25e01ac29ba61a161
brandonlou <14984696+brandonlou@users.noreply.github.com>
2021-07-17 11:35:52 -0700

    Change input to opcode files

--- opcode/preprocess.py
+++ opcode/preprocess.py
@@ -14,38 +14,11 @@ HASH_BITS = 12
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('malign', type=str, help='directory containing malicious pe files')
-    parser.add_argument('benign', type=str, help='directory containing benign pe files')
+    parser.add_argument('malign', type=str, help='directory containing malicious PE opcode files')
+    parser.add_argument('benign', type=str, help='directory containing benign PE opcode files')
     parser.add_argument('-f', '--features', type=str, help='output filename containing pickled features')
     parser.add_argument('-v', '--vectorizer', type=str, help='output filename containing vectorizer used')
     return parser.parse_args()
-
-
-# Extracts opcodes from a single PE file.
-def extract_opcodes(filename: str, prog) -> str:
-    if os.path.getsize(filename) > 2097152: # 2 MiB
-        return None
-    try:
-        objdump = subprocess.run(['objdump', '-d', filename], stdout=subprocess.PIPE, check=True, universal_newlines=True).stdout
-    except:
-        return None
-
-    opcode_sequence = []
-    first_section = False
-
-    for line in objdump.splitlines():
-        # Only want to read one code section for now
-        #if line[0:11] == 'Disassembly':
-        #    if first_section:
-        #        break
-        #    else:
-        #        first_section = True
-        #        continue
-        result = prog.match(line)
-        if result is not None:
-            opcode = result.group(1)
-            opcode_sequence.append(opcode)
-    return ' '.join(opcode_sequence)
 
 
 def main():
@@ -55,24 +28,28 @@ def main():
     features_filename = args.features
     vectorizer_filename = args.vectorizer
 
-    # Any number of whitespace, one or more hex characters, colon, any number of whitespace, two hex characters    
-    prog = re.compile(r"\s+[0-9A-Fa-f]+:\s+([0-9A-Fa-f][0-9A-Fa-f])")
-
     corpus = []
     y = []
-    
-    for pe_file in os.listdir(malign_dir):
-        opcode_sequence = extract_opcodes(f'{malign_dir}/{pe_file}', prog)
-        if opcode_sequence is not None:
+
+    print('Processing opcode files...')   
+ 
+    for root, _, files in os.walk(malign_dir):
+        for f in files:
+            filename = f'{root}/{f}'
+            with open(filename, 'rt') as in_file:
+                opcode_sequence = in_file.read()
             corpus.append(opcode_sequence)
             y.append(1)
-    
-    for pe_file in os.listdir(benign_dir):
-        opcode_sequence = extract_opcodes(f'{benign_dir}/{pe_file}', prog)
-        if opcode_sequence is not None:
+
+    for root, _, files in os.walk(benign_dir):
+        for f in files:
+            filename = f'{root}/{f}'
+            with open(filename, 'rt') as in_file:
+                opcode_sequence = in_file.read()
             corpus.append(opcode_sequence)
             y.append(0)
     
+    print('Vectorizing opcodes...')
     vectorizer = HashingVectorizer(input='content', lowercase=False, stop_words=STOP_WORDS, ngram_range=(2, 2), analyzer='word', n_features=2**HASH_BITS)
     X = vectorizer.fit_transform(corpus)
 
@@ -85,6 +62,8 @@ def main():
         with open(vectorizer_filename, 'wb') as vectorizer_file:
             pickle.dump(vectorizer, vectorizer_file, protocol=pickle.HIGHEST_PROTOCOL)
         print(f'Saved vectorizer to {vectorizer_filename}')
+
+    print('Done')
 
 
 if __name__ == '__main__':
